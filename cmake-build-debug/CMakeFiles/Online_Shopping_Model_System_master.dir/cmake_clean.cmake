3b94ffc4497dd2f143f80eca459039bf0cef35d8
duanfark2 <2643164005@qq.com>
2021-07-04 09:28:32 +0800

    1st

---
+++ cmake-build-debug/CMakeFiles/Online_Shopping_Model_System_master.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Online_Shopping_Model_System_master.dir/LRJS.CPP.obj"
+  "Online_Shopping_Model_System_master.exe"
+  "Online_Shopping_Model_System_master.exe.manifest"
+  "Online_Shopping_Model_System_master.pdb"
+  "libOnline_Shopping_Model_System_master.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Online_Shopping_Model_System_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
